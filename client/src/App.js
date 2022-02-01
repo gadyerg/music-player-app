cb81962b65119fa2fce679975143969a54808549
gadyerg <gadyerg@gmail.com>
2022-02-01 14:18:22 -0700

    added packages and set up server folder

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,16 @@ import React from 'react';
 import Home from './pages/Home';
 import Nav from './components/UI/Nav';
 import { Route, Routes } from 'react-router-dom'; 
+import axios from 'axios';
 
 function App() {
+  async function test() {
+    const data = await axios.get('http://localhost:5000/');
+    console.log(data.data)
+  }
+
+  test();
+
   return (
     <div>
       <Nav />
